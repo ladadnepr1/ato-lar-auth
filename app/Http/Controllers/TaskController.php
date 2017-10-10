8f8f08175891d97fb47497eab5239f86e5f8ea9f
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-10-10 17:44:45 +0300

    TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Task;
 use App\Http\Requests;
 
 class TaskController extends Controller
 {
-    //
+    public function __construct() {
+	$this->middleware('auth');// Проверять авторизацию при доступе
+    }
+    public function index(){
+	$tasks=Task::all();
+	return view ('tasks',['tasks'=>$tasks]);
+    }
+    public function store(Request $request){
+	
+    }
+    public function destroy(Task $task){
+	
+    }
 }
