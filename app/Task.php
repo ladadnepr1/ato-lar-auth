e031cb5031ea72d8bb3854540b409cce8c6934f5
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-10-10 17:30:53 +0300

    linc Task and User create auth

--- app/Task.php
+++ app/Task.php
@@ -13,4 +13,10 @@ class Task extends Model {
      */
     protected $fillable = ['name'];
 
+    /**
+     * Получить пользователя - владельца данной задачи
+     */
+    public function user() {
+	return $this->belongsTo(User::class);
+    }
 }
