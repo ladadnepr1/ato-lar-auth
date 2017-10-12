d2f032a52e89260fbe9c5fdeb79ae39d6b1d6952
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-10-12 16:57:22 +0300

    16-57

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,20 +5,61 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Task;
 use App\Http\Requests;
+use App\Repositories\TaskRepository;
 
-class TaskController extends Controller
-{
-    public function __construct() {
-	$this->middleware('auth');// Проверять авторизацию при доступе
+class TaskController extends Controller {
+
+    /**
+     * Экземпляр TaskRepository.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    public function __construct(TaskRepository $tasks) {
+	$this->middleware('auth'); // Проверять авторизацию при доступе
+	$this->tasks = $tasks;
     }
-    public function index(){
-	$tasks=Task::all();
-	return view ('tasks',['tasks'=>$tasks]);
+
+    /**
+     * Показать список всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+	//$tasks = $request->user()->tasks()->get();
+	//для версии 5.1
+	//$tasks = Task::where('user_id', $request->user()->id)->get();
+
+	return view('tasks.index', [
+	    'tasks' => $this->tasks->forUser($request->user()),
+	]);
     }
-    public function store(Request $request){
-	
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+	$this->validate($request, [
+	    'name' => 'required|max:255',
+	]);
+
+	$request->user()->tasks()->create([
+	    'name' => $request->name,
+	]);
+
+	return redirect('/tasks');
     }
-    public function destroy(Task $task){
-	
+
+    public function destroy(Requests $request, Task $task) {
+	$this->authorize('destroy', $task);
+	$task->delete();
+
+	return redirect('/tasks');
     }
+
 }
